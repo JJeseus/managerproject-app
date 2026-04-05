0025ca4e66c12343874b488ca7a97c3b7a328da4
JJeseus <jimenezsalas45@gmail.com>
2026-04-05 10:42:41 -0600

    Export resource database input types

--- lib/db/types.ts
+++ lib/db/types.ts
@@ -2,15 +2,19 @@ import { z } from 'zod'
 import {
   addCommentInputSchema,
   addNoteInputSchema,
+  addResourceInputSchema,
   addSubtaskInputSchema,
+  createProjectInputSchema,
   createTaskInputSchema,
   deleteTaskInputSchema,
   updateProjectInputSchema,
+  updateResourceInputSchema,
   updateSubtaskInputSchema,
   updateTaskInputSchema,
   updateTaskStatusInputSchema,
 } from './contracts'
 
+export type CreateProjectInput = z.infer<typeof createProjectInputSchema>
 export type CreateTaskInput = z.infer<typeof createTaskInputSchema>
 export type UpdateTaskInput = z.infer<typeof updateTaskInputSchema>
 export type DeleteTaskInput = z.infer<typeof deleteTaskInputSchema>
@@ -18,5 +22,7 @@ export type UpdateTaskStatusInput = z.infer<typeof updateTaskStatusInputSchema>
 export type AddSubtaskInput = z.infer<typeof addSubtaskInputSchema>
 export type UpdateSubtaskInput = z.infer<typeof updateSubtaskInputSchema>
 export type AddCommentInput = z.infer<typeof addCommentInputSchema>
+export type AddResourceInput = z.infer<typeof addResourceInputSchema>
+export type UpdateResourceInput = z.infer<typeof updateResourceInputSchema>
 export type AddNoteInput = z.infer<typeof addNoteInputSchema>
 export type UpdateProjectInput = z.infer<typeof updateProjectInputSchema>
